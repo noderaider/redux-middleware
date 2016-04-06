deeb7cb33e321b52bab88e097f9f0c07d51f441c
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-06 03:00:52 -0700

    done for night

--- src/lib/idleMiddleware.js
+++ src/lib/idleMiddleware.js
@@ -1,19 +1,36 @@
 /** When context has already been created, it can be shared to middleware component. */
+import { createAction } from 'redux-actions'
 
-export const createIdleMiddleware = (context = {}) => {
+const createIdleMiddleware = (context = {}) => {
   //const createDispatcher = configureDispatcher(context)
-  const isIdleAction = ({ meta }) => meta && meta.idle
+  const isIdleAction = ({ type, payload, meta }) => ['IDLEMONITOR_JS_ACTIVITY', 'IDLEMONITOR_JS_ACTIVE', 'IDLEMONITOR_JS_INACTIVE', 'IDLEMONITOR_JS_EXPIRED'].includes(type)
+  let cancel = () => {}
 
   return store => next => action => {
     if(!isIdleAction(action))
       return next(action)
 
-    const { idle } = action.meta
+    //const { idle } = action.meta
     const { dispatch, getState } = store
-    //const dispatcher = createDispatcher(dispatch, getState)
-    if(!actionNames.includes(action.type))
-      return next(action)
+    switch(action.type) {
+      case 'IDLEMONITOR_JS_ACTIVE':
+        //cancel()
+        console.warn('SCHEDULING INACTIVE!')
+        cancel = dispatch(createAction('IDLEMONITOR_JS_INACTIVE', payload => payload, () => { meta: { delay: 6000 } }))
+        break
+      case 'IDLEMONITOR_JS_INACTIVE':
+        //cancel()
+        console.warn('SCHEDULING EXPIRED!')
+        cancel = dispatch(createAction('IDLEMONITOR_JS_EXPIRED', payload => payload, () => { meta: { delay: 6000 } }))
+        break
+      case 'IDLEMONITOR_JS_EXPIRED':
+        dispatch(createAction('FORGET_TOKENS')())
+    }
+
     return next(action)
+  //return { next: 'EXPIRED', delay: 6000 }
+
+  //swal.close()
   }
 }
 
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,6 +2,7 @@ import { applyMiddleware } from 'redux'
 
 //import metaRouter from './metaRouter'
 import timeoutScheduler from './timeoutScheduler'
+import idleMiddleware from './idleMiddleware'
 import thunk from './thunk'
 import readyStatePromise from './readyStatePromise'
 import createLogger from './logger'
@@ -11,8 +12,10 @@ import crashReporter from './crashReporter'
 /** Default array of useful redux middleware */
 export default function middleware() {
   const logger = createLogger()
-  return  [ timeoutScheduler
+  return  [ idleMiddleware
+          , timeoutScheduler
           , thunk
+
           , readyStatePromise
           , logger
           , crashReporter
